cf51fc9a127cc1f11a0474201e736db17c0d5c09
mfaiz-ahmed <faizmaqsood668@gmail.com>
2024-05-21 13:55:10 +0500

    functionality completed

--- todo-app/src/components/TodoAppScreen.tsx
+++ todo-app/src/components/TodoAppScreen.tsx
@@ -21,6 +21,20 @@ export default function TodoAppScreen() {
 
     // DELETE TODO BUTTON
 
+    const deleteTodo = (i:number)=>{
+      textList.splice(i , 1)
+      setTextList([...textList])
+    }
+
+    // EDIT TODO BUTTON
+
+    const editTodo = (i:number)=>{
+      let newText = prompt('Edit your task here')
+      textList.splice(i , 1 , newText)
+      setTextList([...textList])
+    }
+
+
   return (
     <div>
       <input className='me-3' type="text" value={text} placeholder='Enter your tasks here...' onChange={(e)=>{
@@ -29,7 +43,11 @@ export default function TodoAppScreen() {
       <button className='me-3' onClick={addTodo}>Add Todo</button>
       <button onClick={deleteAll}>Delete All</button>
       {textList.map((x:any , i:number)=>{
-        return <p className='mt-3' key={i}>{x}</p>
+        return <p className='mt-3' key={i}>{x} <button onClick={()=>{
+          deleteTodo(i)
+        }}>Delete</button> <button onClick={()=>{
+          editTodo(i)
+        }}>Edit</button> </p>
           
       })}
     </div>
